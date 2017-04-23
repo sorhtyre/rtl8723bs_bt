2636ff613b5cfb3146510e1663f83fb4c73d813d
erythros <erythros@gmail.com>
2017-04-23 12:34:26 -0400

    Remove unneeded commands

--- start_bt.sh
+++ start_bt.sh
@@ -7,17 +7,6 @@ if [ "$1" = "" ]
 then
     # Find the TTY attached to the BT device
     TTY=`dmesg | grep ttyS | cut -b 28-32`
-
-    if [ "$TTY" = "" ]
-    then
-	echo
-	echo "No BT TTY device has been found"
-	echo "Either this computer has no BT device that uses hciattach, or"
-	echo "Your kernel does not have module 8250_dw configured."
-	echo "Note: The configuration variable is CONFIG_SERIAL_8250_DW."
-	echo
-	exit 1
-    fi
 else
     # Use the TTY device mentioned OI the call
     TTY=$1
@@ -26,10 +15,5 @@ fi
 TTY="/dev/$TTY"
 echo "Using device $TTY for Bluetooth"
 
-if [ ! -f /lib/firmware/rtl_bt/rtlbt_config ];
-then
-	mkdir -p /lib/firmware/rtl_bt/
-	cp rtlbt_* /lib/firmware/rtl_bt/.
-fi
 ./rtk_hciattach -n -s 115200 $TTY rtk_h5 > hciattach.txt 2>&1 &
 
